37d4e94397fb0debd11412b69c21278d6b96a5c4
CatGolfbattle <114066657+CatGolfbattle@users.noreply.github.com>
2022-11-13 10:54:38 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+contacts = []
+
+while(True):
+    response = input('(1)add contact (2)print contacts (3)exit: ')
+    
+    if response == '1':
+        person_name = input('Name: ')
+        person_surname = input('Surname: ')
+        person_phone = input('Phone: ')
+        person_email = input('Email: ')
+
+        person_data = {
+            'name': person_name,
+            'surname': person_surname,
+            'phone': person_phone,
+            'email': person_email
+        }
+
+        contacts.append(person_data)
+
+    elif response == '2':
+        for contact in contacts:
+            print("---CONTACT---")
+            print(f"{contact['name']} {contact['surname']}")
+            print(f"Phone: {contact['phone']}")
+            print(f"Email: {contact['email']}")
+
+    elif response == '3':
+        print('Bye bye 😜')
+        exit()
+    else:
+        print('Chose between 1, 2 or 3')
